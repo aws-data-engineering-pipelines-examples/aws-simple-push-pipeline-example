0f3cf903f2b30c353e0a81b87e1fdd3b8b05e056
jszafran <jszafran.pv@gmail.com>
2022-01-08 15:59:58 +0100

    Adding ability to spawn multiple events with multithreading.

--- producers/producer.py
+++ producers/producer.py
@@ -1,7 +1,10 @@
+import argparse
 import datetime
 import random
 import uuid
+from concurrent import futures
 from dataclasses import dataclass
+from time import sleep
 
 SENSOR_IDS = (
     "e970d2fb-d92e-488e-a9bf-8442abd51c9e",
@@ -34,3 +37,40 @@ class Measurement:
             "temperature": self.temperature,
             "epoch": self.epoch,
         }
+
+
+def produce_random_event(max_delay: int) -> None:
+    while True:
+        event = Measurement.create_random_measurement()
+        print(f"Sending {event} event.")
+        sleep(random.randint(1, max_delay))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n-workers",
+        type=int,
+        default=1,
+        help=("Amount of workers that will be spawn to produce random events."),
+    )
+    parser.add_argument(
+        "-max-delay",
+        type=int,
+        default=5,
+        help=(
+            "Maximum wait time (in seconds) before new event is produced by worker. "
+            "Randomly picked from range 1 to -max-delay arg value."
+        ),
+    )
+
+    args = parser.parse_args()
+
+    fn_args = (args.max_delay for _ in range(args.n_workers))
+
+    print(
+        f"Starting {args.n_workers} worker(s) with {args.max_delay} seconds of max delay between producing new event."
+    )
+
+    with futures.ThreadPoolExecutor(max_workers=args.n_workers) as executor:
+        executor.map(produce_random_event, fn_args)
